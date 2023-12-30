a680a7e7ff8c67d3e305d51be4273e7a36788826
Samith <samithwijesinha@gmail.com>
2023-12-30 11:23:02 +0530

    adding main and duck classes - stratergy pattern

---
+++ Java_Examples/Strategy pattern/Duck.java
@@ -0,0 +1,15 @@
+public class Duck {
+
+    public void quack() {
+        System.out.println("quack");
+    }
+
+    public void swim() {
+        System.out.println("swim")
+    }
+
+    public void display() {
+        System.out.println("display")
+    }
+
+}
