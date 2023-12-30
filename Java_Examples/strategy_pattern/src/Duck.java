3ffc1d1cbf79f8497d59b9705caf9b39b2220158
Samith <samithwijesinha@gmail.com>
2023-12-30 11:45:16 +0530

    [stratergy pattern] - basic use case of the duck example

---
+++ Java_Examples/strategy_pattern/src/Duck.java
@@ -0,0 +1,11 @@
+public class Duck {
+    public void quack() {
+        System.out.println("quack!");
+    }
+    public void swim(){
+        System.out.println("Swimming");
+    }
+    public void display(){
+        System.out.println("I am a duck");
+    }
+}
